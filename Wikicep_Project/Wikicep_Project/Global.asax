d5920e6cbdb89e849be0f075c1cfbd9d80560d76
SOFRECOM\FRamundo <federicoramundo5@gmail.com>
2016-12-19 10:30:33 -0300

    Commit proyecto

---
+++ Wikicep_Project/Wikicep_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wikicep_Project.MvcApplication" Language="C#" %>
---
+++ Wikicep_Project/Wikicep_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wikicep_Project.Startup))]
+namespace Wikicep_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
